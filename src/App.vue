cbd64cef52a5d8365353b72812d922e472d83ef1
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-06-15 15:43:27 -0300

    add imc component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ImcCalc/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImcCalc from './components/ImcCalc.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ImcCalc
   }
 }
 </script>
---
+++ src/components/ImcCalc.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="imc-inputs">
+    <input type="number" v-model="user_height" class="height" placeholder="Height (cm)">
+    <input type="number" v-on:input="calculate" v-model="user_weight" class="weight" placeholder="Weight (kg)">
+    <button v-on:click="calculate">See IMC</button>
+    <h2>Your BMI is {{user_imc}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ImcCalc',
+    proprs: [],
+    data(){
+      return {
+        user_height: "",
+        user_weight: "",
+        user_imc: ""
+      }
+    }, 
+    methods: {
+      calculate(){
+        this.user_imc = 'Your IMC is '+(this.user_weight / (Math.pow(this.user_height, 2))).toString().substring(4, 6)
+      },
+
+      category(){
+        // func
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
