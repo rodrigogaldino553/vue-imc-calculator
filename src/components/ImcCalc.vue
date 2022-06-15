17896f50cc7ed3d10596894f23fc21677e8126be
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-06-15 15:59:45 -0300

    style imc component

--- src/components/ImcCalc.vue
+++ src/components/ImcCalc.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="imc-inputs">
-    <input type="number" v-model="user_height" class="height" placeholder="Height (cm)">
-    <input type="number" v-on:input="calculate" v-model="user_weight" class="weight" placeholder="Weight (kg)">
-    <button v-on:click="calculate">See IMC</button>
-    <h2>Your BMI is {{user_imc}}</h2>
+    <h1>Are you health?</h1>
+    <input type="number" v-model="user_height" class="height" placeholder="cm">
+    <input type="number" v-on:input="calculate" v-model="user_weight" class="weight" placeholder="kg">
+    <!-- button v-on:click="calculate">See IMC</button-->
+    <h2>{{user_imc}}</h2>
   </div>
 </template>
 
@@ -31,4 +32,27 @@
 </script>
 
 <style scoped>
+  .imc-inputs{
+    border-radius: 4px;
+    box-shadow: 2px 2px 5px gray;
+    max-width: 300px;
+    padding: 20px;
+    margin: auto;
+  }
+
+  .imc-inputs input{
+    max-width: 50px;
+    max-height: 70px;
+    font-size: 28px;
+    padding: 15px 5px;
+    border-radius: 16px;
+    border:none;
+    background: gainsboro;
+    text-align: center;
+    margin-left: 4px;
+  }
+
+  .imc-inputs .height{
+    max-width: 70px;
+  }
 </style>
