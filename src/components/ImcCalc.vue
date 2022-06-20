3cc39937b919bb5e77a0661153e158c5f752d673
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-06-20 10:42:12 -0300

    fix fields

--- src/components/ImcCalc.vue
+++ src/components/ImcCalc.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="imc-inputs">
     <h1>Are you health?</h1>
-    <input type="number" v-on:input="calculate" v-model="user_height" class="height" placeholder="cm">
-    <input type="number" v-on:input="calculate" v-model="user_weight" class="weight" placeholder="kg">
+    <div class="fields">
+      <div class="field">
+        <label for="height">Height</label>
+        <input name="height" type="number" v-on:input="calculate" v-model="user_height" class="height" placeholder="cm">
+      </div>
+      <div class="field">
+        <label for="weight">Weight</label>
+        <input name="weight" type="number" v-on:input="calculate" v-model="user_weight" class="weight" placeholder="kg">
+      </div>
+    </div>
     <!-- button v-on:click="calculate">See IMC</button-->
     <h2>{{user_imc}}</h2>
   </div>
@@ -44,7 +52,25 @@
     margin: auto;
   }
 
-  .imc-inputs input{
+  .imc-inputs .fields{
+    display: flex;
+    justify-content: center;
+  }
+
+  .imc-inputs .fields .field{
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    max-width: fit-content;
+  }
+
+  .imc-inputs .fields .field label{
+    width: min-content;
+    font-size: 12px;
+  }
+
+  .imc-inputs .fields .field input{
     max-width: 50px;
     max-height: 70px;
     font-size: 28px;
@@ -54,9 +80,11 @@
     background: gainsboro;
     text-align: center;
     margin-left: 4px;
+    font-weight: 600;
+    color: #2c3e50;
   }
 
   .imc-inputs .height{
-    max-width: 70px;
+    min-width: 70px;
   }
 </style>
